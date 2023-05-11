f0a889b67de2addf72569e8a4222fb3680c72c6e
Quang Vinh <qvinh9034@gmail.com>
2023-05-11 21:51:41 +0700

    Component 1

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+export default class footer extends  Component{
+  render(){
+    return (
+     <div>
+      <p>copyright @ 2022</p>
+     </div>
+    )
+  }
+}
